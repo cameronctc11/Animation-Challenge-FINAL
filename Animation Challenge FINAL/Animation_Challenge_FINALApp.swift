2c6527f8eccba2d62130bd7bab314ed41e1b591c
cameronmckenzie <cameronctc11@gmail.com>
2025-09-25 13:41:47 -0600

    Initial Commit

---
+++ Animation Challenge FINAL/Animation_Challenge_FINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_Challenge_FINALApp.swift
+//  Animation Challenge FINAL
+//
+//  Created by McKenzie, Cameron - Student on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Animation_Challenge_FINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation Challenge FINALTests/Animation_Challenge_FINALTests.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_Challenge_FINALTests.swift
+//  Animation Challenge FINALTests
+//
+//  Created by McKenzie, Cameron - Student on 9/25/25.
+//
+
+import Testing
+@testable import Animation_Challenge_FINAL
+
+struct Animation_Challenge_FINALTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
